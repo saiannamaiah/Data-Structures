dcdb96255745a95ba911d5eeb12cc3b3c5d0fdde
SaiAnnamaiahB <annamaiahsai@gmail.com>
2025-02-05 16:04:41 +0530

    Arrays Ititiliazise

---
+++ src/Array.java
@@ -0,0 +1,15 @@
+public class Array {
+
+    private int[] items;
+    int count = 0;
+
+    public Array(int length) {
+        items = new int[length];
+    }
+
+    public void print() {
+        for (int i = 0; i < count; i++) {
+            System.out.println(items[i]);
+        }
+    }
+}
